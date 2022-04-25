94b2d2a8e0bedf1598d9b1f2b2d3ebfbb783b7ea
randytbushman <randytbushman@gmail.com>
2022-04-25 19:16:11 -0400

    Documentation

--- PacketReceiver.py
+++ PacketReceiver.py
@@ -3,11 +3,19 @@ import argparse
 import time
 
 
-def decodePacket(packet):
-    return chr(packet[12])
+def decodePacket(payload):
+    """
+    Decodes the RTP packet payload and returns the encoded character.
+    @return: the encoded character in the RTP packet
+    """
+    return chr(payload[12])
 
 
 def get_ip():
+    """
+    Returns the host IP of this host.
+    @return: a string representation of the ipv4 IP address
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
     try:
@@ -21,6 +29,12 @@ def get_ip():
 
 
 def startListener(sock, stealth):
+    """
+    Begins the listener and returns the information decoded from the packets.
+    @param sock: the python socket object that the listener operates on
+    @param stealth: the offset stealth value of the packet encodings
+    @return: the decoded message
+    """
     message = ""
     receivedPackets = 0
     while True:
@@ -28,10 +42,18 @@ def startListener(sock, stealth):
         if receivedPackets == 0:
             message += decodePacket(data)
         receivedPackets = (1 + receivedPackets) % stealth
+    sock.close()
     return message
 
 
 def startListenerV(sock, stealth):
+    """
+    Begins the listener in verbose mode 1 and returns the information decoded from the packets. Each decoded character
+    is printed to the standard output.
+    @param sock: the python socket object that the listener operates on
+    @param stealth: the offset stealth value of the packet encodings
+    @return: the decoded message
+    """
     message = ""
     receivedPackets = 0
     while True:
@@ -41,11 +63,18 @@ def startListenerV(sock, stealth):
             message += char
             print(char, end="")
         receivedPackets = (1 + receivedPackets) % stealth
+    sock.close()
     return message
 
 
 def startListenerVV(sock, stealth):
-    print("vv mode")
+    """
+    Begins the listener in verbose mode 2 and returns the information decoded from the packets. Each decoded character
+    is printed to the standard output along with the time it takes for each packet to arrive.
+    @param sock: the python socket object that the listener operates on
+    @param stealth: the offset stealth value of the packet encodings
+    @return: the decoded message
+    """
     message = ""
     receivedPackets = 0
     i = 0
@@ -56,12 +85,21 @@ def startListenerVV(sock, stealth):
             char = decodePacket(data)
             message += char
             i += 1
-            print(char, f"\t\tDecoded Packets #: {i}", f"\t\tTimestamp: {time.time() - startTime}s")
+            print(char, f"\t\tDecoded Packets #: {i}", f"\t\tTimestamp: {time.time() - startTime}")
         receivedPackets = (1 + receivedPackets) % stealth
+    sock.close()
     return message
 
 
 def startListenerVVV(sock, stealth):
+    """
+    Begins the listener in verbose mode 3 and returns the information decoded from the packets. Each decoded character
+    is printed to the standard output along with the time it takes for each packet to arrive. The payload is also
+    printed.
+    @param sock: the python socket object that the listener operates on
+    @param stealth: the offset stealth value of the packet encodings
+    @return: the decoded message
+    """
     message = ""
     receivedPackets = 0
     i = 0
@@ -72,20 +110,21 @@ def startListenerVVV(sock, stealth):
             char = decodePacket(data)
             message += char
             i += 1
-            print(char, f"\t\tDecoded Packets #: {i}", f"\t\tTimestamp: {time.time() - startTime}s")
-            print(f"Packets #{i} Data:\n {data}\n")
+            print(char, f"\t\tDecoded Packets #: {i}", f"\t\tTimestamp: {time.time() - startTime}")
+            print(f"Packet {i}: {data}\n\n")
         receivedPackets = (1 + receivedPackets) % stealth
+    sock.close()
     return message
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--listener_port", required=True, help="The port we listen on.")
-    parser.add_argument("-s", "--stealth", default=1, help="Number of packets skipped before next RTP encoding.")
-    parser.add_argument("-o", "--output", help="Output file location of message.")
-    parser.add_argument("-v", "--verbose", help="Verbose output.", action='store_true')
-    parser.add_argument("-vv", "--verbose2", help="More verbose output.", action='store_true')
-    parser.add_argument("-vvv", "--verbose3", help="Most verbose output.", action='store_true')
+    parser.add_argument("-p", "--listener_port", help="The port we listen on")
+    parser.add_argument("-s", "--stealth", help="Number of packet encodings between each RTP transmission")
+    parser.add_argument("-o", "--output", help="Output file location of message")
+    parser.add_argument("-v", "--verbose", help="Prints each character to the stdout as it arrives", action='store_true')
+    parser.add_argument("-vv", "--vverbose", help="Prints each character to the stdout as it arrives", action='store_true')
+    parser.add_argument("-vvv", "--vvverbose", help="Prints each character to the stdout as it arrives", action='store_true')
     args = parser.parse_args()
 
     # Parse the arguments from the command line
@@ -93,21 +132,21 @@ if __name__ == '__main__':
     stealth = int(args.stealth)
     output = args.output
 
-    UDP_IP = get_ip()
-    UDP_PORT = int(args.listener_port)
+    # Open and bind socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    sock.bind((UDP_IP, UDP_PORT))
+    sock.bind((get_ip(), int(args.listener_port)))
 
-    if args.verbose3:
+    # Select the listener function depending on verbosity value
+    if args.vvverbose:
         message = startListenerVVV(sock, stealth)
-    elif args.verbose2:
+    elif args.vverbose:
         message = startListenerVV(sock, stealth)
     elif args.verbose:
         message = startListenerV(sock, stealth)
     else:
         message = startListener(sock, stealth)
 
-    sock.close()
+    # Save the message to the specified output location
     if output is not None:
         f = open(output, 'w')
         f.write(message)
